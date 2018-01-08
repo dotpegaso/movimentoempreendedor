11fccefbcecef7421662398180cc39110645c0d6
André L Benatti <dotpegaso@gmail.com>
2018-01-08 23:45:44 +0000

    removed instagram factory

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute', 'home', 'about', 'jtt_instagram'])
+angular.module('app', ['ngRoute', 'home', 'about'])
 
 .config(function($routeProvider, $locationProvider){
     
@@ -19,18 +19,3 @@ angular.module('app', ['ngRoute', 'home', 'about', 'jtt_instagram'])
         $locationProvider.html5Mode(true);
     
 })
-
-.controller('controller', ['$scope', 'instagramFactory', function($scope, instagramFactory) {
-
-    var _access_token = '5896353959.13f803e.0d72777f26e94236b448e3a5c2e9aa06';
-
-
-    // user id converter: http://jelled.com/instagram/lookup-user-id
-    instagramFactory.getMediaFromUserById({
-        userId:"5896353959",
-        access_token:_access_token,
-    }).then(function(_data){
-        console.info("media from user by id", _data);
-    });
-
-}]);

5d2bda4ddb02a43f9ac6ee6208f7dfe7ac931d9d
André L Benatti <dotpegaso@gmail.com>
2018-01-08 23:43:14 +0000

    add instagram factory

--- modules/home/js/homeController.js
+++ modules/home/js/homeController.js
@@ -2,10 +2,15 @@ angular.module('home', [])
 
 .controller('homeController', function($scope, $http){
   
-    
-    
-    $scope.submit = function(){
-      $http.post('/src/global/mailChimp.php', this.mailChimp);
-    }
+  function fnSuccess (res) {
+     res.data ? $scope.success = true : $scope.error = true;
+  }
+  
+  $scope.submit = function() {
+    return $http.post('/src/global/mailChimp.php', this.mailChimp).then(function(success) {
+      return fnSuccess(success);
+    });
+  }
+  
     
 })
